7b35d2ae0249e4b6a057ab91a0e08e5b763ef666
zhedh <webdahua@163.com>
2024-04-17 15:25:15 +0800

    update: pm2 应用存活校验

--- start.sh
+++ start.sh
@@ -42,7 +42,12 @@ done < $CLOUDTOP_ENV
 
 
 # 停止正在运行的项目
-pm2 stop all
+pm2_list=$(pm2 list)
+
+if [[ $pm2_list == *"online"* ]]; then
+  pm2 stop all
+fi
+
 
 # 启动 cloudtop_server 项目
 cd cloudtop_server && npm run start && cd ../ &&
