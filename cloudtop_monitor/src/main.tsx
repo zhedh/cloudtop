24f7bc17e8ef96e0ba64911b351da38523ff0a15
zhedh <webdahua@163.com>
2024-11-21 14:35:42 +0800

    fix: 修改启动脚本逻辑， 兼容mac与linux

--- cloudtop_monitor/src/main.tsx
+++ cloudtop_monitor/src/main.tsx
@@ -5,41 +5,6 @@ import { ConfigProvider } from 'antd'
 import dayjs from 'dayjs'
 import zhCN from 'antd/locale/zh_CN'
 import 'dayjs/locale/zh-cn'
-
-// const xhrSend = XMLHttpRequest.prototype.send
-
-// XMLHttpRequest.prototype.send = function (body) {
-//   console.log('body:: ', body)
-//   this.addEventListener('load', (e) => {
-//     console.log('XMLHttpRequest: ', e)
-//     const target = e.target as XMLHttpRequest
-
-//     const path = target.responseURL?.split('?')[0]
-//     const reg = /https?:\/\/.+\/.*\.([^/.]+)$/
-
-//     console.log('path::: ', path)
-//     console.log(reg.test(path))
-//     console.log(reg.test('http://www.baidu.com'))
-//   })
-//   xhrSend.call(this, body)
-// }
-
-// const _fetch = window.fetch
-
-// window.fetch = (input: RequestInfo | URL, init?: RequestInit) => {
-//   return _fetch(input, init).then(
-//     (res) => {
-//       console.log('window.fetch: ', res)
-//       // handle(res)
-//       return res
-//     },
-//     (res) => {
-//       // handle(res)
-//       return Promise.reject(res)
-//     }
-//   )
-// }
-
 import App from './App.tsx'
 import './index.css'
 
